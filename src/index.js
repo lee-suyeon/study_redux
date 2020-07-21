cc56d7120e28b955d6c6d109f0c1231b87984fc2
lee-suyeon <42996064+sroovy@users.noreply.github.com>
2020-07-21 16:01:20 +0900

    Add store and Provider
    1. createStore에 rootReducer를 넣어 store를 만든다.
    2. provider를 불러와 app 컴포넌트를 감싸주면 하위 컴포넌트는 store에 접근할 수 있게 된다.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './exercise';
+import { createStore } from 'redux';
+import rootReducer from './modules';
+import { Provider } from 'react-redux';
 
+const store = createStore(rootReducer); // 스토어를 만든다. 
+console.log(store.getState()); // 스토어의 상태 확인
+//counter: {number: 0, diff: 1}
+//todos: []
+
+// Provider로 store를 넣어서 App 을 감싸게 되면 
+// 렌더링하는 그 어떤 컴포넌트던지 리덕스 스토어에 접근 할 수 있게 된다. 
 
 ReactDOM.render(
-  //<React.StrictMode>
+  <Provider store={store}>
     <App />,
-  //</React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
