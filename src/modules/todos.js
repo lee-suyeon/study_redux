4b3e92b1b198ddcb2e05a3a4b09a6361716a63a8
lee-suyeon <42996064+sroovy@users.noreply.github.com>
2020-07-21 15:41:46 +0900

    Create redux module _ sub reducer
    1. counter 모듈과 todos 모듈을 만든다.

---
+++ src/modules/todos.js
@@ -0,0 +1,46 @@
+// 액션 타입 선언
+const ADD_TODO = 'todos/ADD_TODO';
+const TOGGLE_TODO = 'todos/TOGGLE_TODO';
+
+// 액션 생성 함수 선언
+let nextId = 1; // todo 데이터에서 사용 할 고유 id
+export const addTodo = (text) => ({ 
+    type: ADD_TODO,
+    todo : {
+        id: nextId++,
+        text
+    }
+});
+export const toggleTodo = (id) => ({ 
+    type: TOGGLE_TODO,
+    id
+});
+
+
+// 초기 상태 선언
+const initialState = [
+    /*
+    {
+        id: 1,
+        text: 'example',
+        done: false
+    }
+    */
+];
+
+// 리듀서 선언
+export default function todos(state = initialState, action) {
+    switch(action.type) {
+        case ADD_TODO:
+            return state.concat(action.todo);
+        case TOGGLE_TODO:
+            return state.map(
+                todo =>
+                    todo.id === action.id // id가 일치하면
+                    ? {...todo, done: !todo.done} // done 값을 반전시키고
+                    : todo // 아니라면 그대로 둔다. 
+            );
+        default:
+            return state;
+    }
+}
