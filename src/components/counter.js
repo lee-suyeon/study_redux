d42e3af54297291aaaece4bd626fbf9376dbb681
lee-suyeon <42996064+sroovy@users.noreply.github.com>
2020-07-22 14:37:30 +0900

    initial commit
    react-redux-middleware-study

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,19 +1,12 @@
 import React from 'react';
 
-function Counter ({ number, diff, onIncrease, onDecrease, onSetDiff }) {
-    const onChange = (e) => {
-        // e.target.value의 타입은 문자열이기 때문에 숫자로 변환해주어야 한다. 
-        onSetDiff(Number(e.target.value));
-    };
+function Counter({ number, onIncrease, onDecrease }){
     return (
-        <>
+        <div>
             <h1>{number}</h1>
-            <div>
-                <input type="number" value={diff} min="1" onChange={onChange} />
-                <button onClick={onIncrease}>+</button>
-                <button onClick={onDecrease}>-</button>
-            </div>
-        </>
+            <button onClick={onIncrease}>+</button>
+            <button onClick={onDecrease}>-</button>
+        </div>
     );
 }
 
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,10 +1,7 @@
+// root Reducer 
 import { combineReducers } from 'redux';
 import counter from './counter';
-import todos from './todos';
 
-const rootReducer = combineReducers({
-    counter, 
-    todos
-});
+const rootReducer = combineReducers({ counter });
 
 export default rootReducer;
